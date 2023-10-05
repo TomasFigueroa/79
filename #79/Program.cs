8bb4fabd1b9dd1682591b2798fd6d3cc62585b67
dstomasfigueroa@hotmail.com <dstomasfigueroa@hotmail.com>
2023-10-04 22:11:46 -0300

    Agregar archivos de proyecto.

---
+++ #79/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> frasesHashSet = new HashSet<string>();
+        SortedSet<string> frasesSortedSet = new SortedSet<string>();
+
+        while (true)
+        {
+            Console.Write("Introduce una frase (o deja vacío para salir): ");
+            string frase = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(frase))
+                break;
+
+            if (frasesHashSet.Contains(frase))
+            {
+                Console.WriteLine("Esta frase ya fue introducida anteriormente.");
+            }
+            else
+            {
+                frasesHashSet.Add(frase);
+                frasesSortedSet.Add(frase);
+                Console.WriteLine("Frase agregada al conjunto.");
+            }
+        }
+
+        Console.WriteLine("\nFrases introducidas en orden alfabético:");
+        foreach (string frase in frasesSortedSet)
+        {
+            Console.WriteLine(frase);
+        }
+    }
+}
